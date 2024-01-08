8b5676af0245d1f5577e102f1131022e06211647
Marco von Raumer <marco.von.raumer@digitecgalaxus.ch>
2024-01-08 14:32:29 +0100

    Initial commit

---
+++ debug.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+docker build -t app:7 -f Dockerfile_7 .
+docker build -t app:8 -f Dockerfile_8 .
+
+
+docker run -d -e COMPlus_EnableDiagnostics=0 --name app app:7
+sleep 2
+docker exec -it app datadog/dd-dotnet.sh check process 1
+docker rm -f app
+
+
+docker run -d --name app app:7
+sleep 2
+docker exec -it app datadog/dd-dotnet.sh check process 1
+docker rm -f app
+
+
+docker run -d -e COMPlus_EnableDiagnostics=0 --name app app:8
+sleep 2
+docker exec -it app datadog/dd-dotnet.sh check process 1
+docker rm -f app
+
+
+docker run -d --name app app:8
+sleep 2
+docker exec -it app datadog/dd-dotnet.sh check process 1
+docker rm -f app
