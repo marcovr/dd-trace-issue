02d568751e94b6c585457966cba53c5e5f1a3fa4
Marco von Raumer <marco.von.raumer@digitecgalaxus.ch>
2024-01-08 14:35:32 +0100

    fix

--- debug.sh
+++ debug.sh
@@ -5,23 +5,23 @@ docker build -t app:8 -f Dockerfile_8 .
 
 docker run -d -e COMPlus_EnableDiagnostics=0 --name app app:7
 sleep 2
-docker exec -it app datadog/dd-dotnet.sh check process 1
+docker exec app datadog/dd-dotnet.sh check process 1
 docker rm -f app
 
 
 docker run -d --name app app:7
 sleep 2
-docker exec -it app datadog/dd-dotnet.sh check process 1
+docker exec app datadog/dd-dotnet.sh check process 1
 docker rm -f app
 
 
 docker run -d -e COMPlus_EnableDiagnostics=0 --name app app:8
 sleep 2
-docker exec -it app datadog/dd-dotnet.sh check process 1
+docker exec app datadog/dd-dotnet.sh check process 1
 docker rm -f app
 
 
 docker run -d --name app app:8
 sleep 2
-docker exec -it app datadog/dd-dotnet.sh check process 1
+docker exec app datadog/dd-dotnet.sh check process 1
 docker rm -f app
